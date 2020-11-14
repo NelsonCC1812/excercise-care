95230da4cad5fb491995e5834cbdf2f8cc7bca81
nelson1812 <nelsoncc1812@gmail.com>
2020-11-14 17:06:18 +0000

    Deploy

---
+++ server/public/precache-manifest.7c0b1ce31a6e988bc2f6250fcbb186bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4180d57ec39937c9a32c58e82893c237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cee265b1d1457124c0e5",
+    "url": "/static/css/main.e7358628.chunk.css"
+  },
+  {
+    "revision": "f8a6243434803e5fd7fa",
+    "url": "/static/js/2.ac86086f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ac86086f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee265b1d1457124c0e5",
+    "url": "/static/js/main.ff1ef0b4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c9d61f0565d1994dc1783ecc4fb65190",
+    "url": "/static/media/menu.c9d61f05.svg"
+  }
+]);
